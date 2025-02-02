589da6b51e00ebbe36bc60a284e97d611d946cf7
christopher <christopher.colleran@studentfr.ch>
2025-02-02 21:05:44 +0100

    devoir01 fini

--- src/app/Application.java
+++ src/app/Application.java
@@ -1,17 +1,51 @@
 package app;
 
 public class Application {
-    
-        //
-        //              )        (                 ) (           (         (
-        //           ( /(   *   ))\ )        (  ( /( )\ )        )\ )  (   )\ )
-        //     (   ( )\())` )  /(()/((       )\ )\()|()/(  (    (()/(  )\ (()/(
-        //     )\  )((_)\  ( )(_))(_))\    (((_|(_)\ /(_)) )\    /(_)|((_) /(_))
-        //    ((_)((_)((_)(_(_()|_))((_)   )\___ ((_|_))_ ((_)  (_)) )\___(_))
-        //    __   _____ _____ ___ ___    ___ ___  ___  ___   ___ ___ ___   _
-        //    \ \ / / _ \_   _| _ \ __|  / __/ _ \|   \| __| |_ _/ __|_ _| | |
-        //     \ V / (_) || | |   / _|  | (_| (_) | |) | _|   | | (__ | |  |_|
-        //      \_/ \___/ |_| |_|_\___|  \___\___/|___/|___| |___\___|___| (_)
-        //
+        public static final String ami1 = "jackie";
+        public static final String ami2 = "niilo";
+        public static final String ami3 = "christopher";
 
+        public static void main(String[] args) {
+                System.out.println("");
+                System.out.println("");
+                Ami Ami1 = new Ami();
+                Ami Ami2 = new Ami();
+                Ami Ami3 = new Ami();
+                boolean condition = true;
+                do {
+                        System.out.println("prochain round : ");
+                        if (Ami1.x == 0) {
+                                System.out.println(ami1 + " est complètement bourré!");
+                        } else {
+                                System.out.println(ami1 + " bières restants : " + Ami1.x);
+                        }
+                        if (Ami2.x == 0) {
+                                System.out.println(ami2 + " est complètement bourré!");
+                        } else {
+                                System.out.println(ami2 + " bières restants : " + Ami2.x);
+                        }
+                        if (Ami3.x == 0) {
+                                System.out.println(ami3 + " est complètement bourré!");
+                        } else {
+                                System.out.println(ami3 + " bières restants : " + Ami3.x);
+                        }
+                        if (Ami1.x == 0 & Ami2.x == 0 & Ami3.x == 0) {
+                                condition = false;
+                        }
+                        boit(Ami3);
+                        boit(Ami2);
+                        boit(Ami1);
+                        System.out.println("");
+                        System.out.println("");
+
+                } while (condition);
+        }
+
+        public static int boit(Ami ami) {
+                ami.x -= 1;
+                if (ami.x < 0) {
+                        ami.x = 0;
+                }
+                return ami.x;
+        }
 }
